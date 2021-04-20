9cfd49e27c3a8c9eb7484858960171070e16eee4
ProgramistycznySwir <programistycznyswir@gmail.com>
2021-04-20 13:29:03 +0200

    commit

--- Zadanie1.c
+++ Zadanie1.c
@@ -18,17 +18,20 @@ int main ()
     int th1_result = pthread_create(&th1, NULL, Increment, NULL);
 
     printf("Exitted main thread.\n");
+    printf("Thread statuses: {th0: %d, th1: %d}", th0_result, th1_result);
     return 0;
 }
 
 void* Display (void* args)
 {
+    printf("Started Display()\n");
     while(1)
         printf("%d\n", Licznik);
 }
 
 void* Increment (void* args)
 {
+    printf("Started Increment()\n");
     while(1)
         Licznik++;
 }
