53bb5fe6b53d0b1a87432bc5bde49998764d916e
ProgramistycznyŚwir <programistycznyswir@gmail.com>
2021-04-20 11:29:01 +0200

    commit

--- Zadanie1.c
+++ Zadanie1.c
@@ -8,14 +8,18 @@ int Licznik = 0;
 int main ()
 {
     pthread_t th0, th1;
+    int pthread_create(pthread_t* th0, pthread_attr_t attr, void* (*start_routine)(void*) Display, void* arg);
+    int pthread_create(pthread_t* th1, pthread_attr_t attr, void* (*start_routine)(void*), void* arg);
 }
 
 void Display (void* args)
 {
-    printf("%d\n", Licznik);
+    while(true)
+        printf("%d\n", Licznik);
 }
 
 void Increment (void* args)
 {
-    Licznik++;
+    while(true)
+        Licznik++;
 }
