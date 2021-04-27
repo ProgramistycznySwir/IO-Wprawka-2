9e641d4a9c49506774e82fca47c82c170dbf3050
ProgramistycznyŚwir <programistycznyswir@gmail.com>
2021-04-27 09:25:08 +0200

    Added Zadanie1

--- Zadanie1.c
+++ Zadanie1.c
@@ -11,14 +11,33 @@ void* Increment (void* args);
 int main ()
 {
     pthread_t th0, th1;
-    pthread_attr_t attr;
+    // pthread_attr_t attr;
     char* arg = "gah";
     // int th0_result = pthread_create(&th0, NULL, Display, (void*) arg);
     int th0_result = pthread_create(&th0, NULL, Display, NULL);
     int th1_result = pthread_create(&th1, NULL, Increment, NULL);
 
-    printf("Exitted main thread.\n");
+    if(th0_result || th1_result)
+    {
+        printf("Error when attempting to create new thread!\n");
+        exit(-1);
+    }
+
+
+    void* status;
+    int th0_result = pthread_join(th0, &status);
+    int th1_result = pthread_join(th1, &status);
+
+    if(th0_result || th1_result)
+    {
+        printf("Error when attempting to join new threads!\n");
+        exit(-1);
+    }
+
+
+    printf("Successfully exitted main thread.\n");
     return 0;
+    // pthread_exit(NULL);
 }
 
 void* Display (void* args)
